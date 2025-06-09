2896798cac41814f427d42c853d89a9eacc2d332
Sujeet17m <sujeet17das@gmail.com>
2025-06-09 15:30:45 +0530

    mm

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
-// require('./xyz');// one module into another
-
-import { calculateSum } from "./sum.js";
-// const { calculateSum , x} = require('./sum');
+require('./xyz');// one module into another
+const { calculateSum , x} = require('./sum');
 
 
 
 var name = "Namaste node js";
 var a = 10;
 var b = 20;
-var x = 100;
-
 
 calculateSum(a,b);
 console.log(x);
-
-// console.log(x);
 
 
 console.log(globalThis === global);
--- sum.js
+++ sum.js
@@ -1,15 +1,16 @@
-console.log("sum module executed");
-
 var x = "Hello World";
 
-export function calculateSum(a ,b){
+function calculateSum(a ,b){
     const sum = a + b;
 
     console.log(sum);
     
 }
 
-// module.exports = {
-//     x ,
-//     calculateSum
-// };
+module.exports = {
+    x ,
+    calculateSum
+};
+
+// module.exports.x = x;
+// module.exports.calculateSum = calculateSum;
